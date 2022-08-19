2247a519561de84e562ca639dd4dff4f1a8caf19
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-19 13:10:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(Scuber){
+  
+  if (Scuber <= 400){
+    return 'This one is on me!'
+  }
+  else if(Scuber > 2000 && Scuber < 2500)
+  {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else 
+  {return 'No can do.'}
+  };
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
+  return city == 'NYC' ?'Ok, sounds good.' : 'No go.'
+};
+
+function switchOnCharmFromTip(charm){
   // Write your code here!
+switch (charm){
+  case 'generous':
+    return "Thank you so much."
+
+    case 'not as generous':
+      return "Thank you."
+
+      default:
+        return "Bye."
 }
+};
